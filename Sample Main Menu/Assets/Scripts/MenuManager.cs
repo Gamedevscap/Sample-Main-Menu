10ed096f37238aadaa311e6c16097b456da39343
Gamedevscap <gamedevscap@users.noreply.github.com>
2018-10-30 18:19:02 +0100

    Added the "Back"function by pressing B button (controller) or Escape (keyboard)

--- Sample Main Menu/Assets/Scripts/MenuManager.cs
+++ Sample Main Menu/Assets/Scripts/MenuManager.cs
@@ -2,9 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour {
+
+    /* ---
+    ATTACH THIS SCRIPT 
+    TO THE EVENT SYSTEM GAMEOBJECT 
+    --- */
+
+    [Header("Main Menu Composition")]
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject selectLevelMenu;
+    [SerializeField] GameObject settingsMenu;
+    [SerializeField] GameObject infoMenu;
+
+    [Header("First Selected Buttons")]
+    [SerializeField] GameObject startButton;
+    [SerializeField] GameObject selectLevelButton;
+    [SerializeField] GameObject settingsButton;
+
+    EventSystem es;
+
+    private void Start()
+    {
+        es = GetComponent<EventSystem>();
+    }
+
+    private void Update()
+    {
+        BackToMainMenu();
+        //FirstSelected();
+    }
 
     public void StartScene(string sceneName)
     {
@@ -14,5 +43,49 @@ public class MenuManager : MonoBehaviour {
 	public void ExitGame()
     {
         Application.Quit();
+    }
+
+    //--- Modify the event system to change the first selected button (usefull for the controller input) ---//
+
+    /*void FirstSelected()
+    {
+        if (mainMenu.gameObject.activeSelf == true)
+        {
+            es.SetSelectedGameObject(startButton);
+        }
+
+        if (selectLevelMenu.gameObject.activeSelf == true)
+        {
+            es.SetSelectedGameObject(selectLevelButton);
+        }
+
+        if (settingsMenu.gameObject.activeSelf == true)
+        {
+            es.SetSelectedGameObject(settingsButton);
+        }
+    }*/
+
+    //--- Conditions to use the "B" button or "Escape" button to back to the main menu ---//
+
+    void BackToMainMenu()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            if (mainMenu.gameObject.activeSelf == false && selectLevelMenu.gameObject.activeSelf == true)
+            {
+                selectLevelMenu.gameObject.SetActive(false);
+                mainMenu.gameObject.SetActive(true);
+            }
+            if (mainMenu.gameObject.activeSelf == false && settingsMenu.gameObject.activeSelf == true)
+            {
+                settingsMenu.gameObject.SetActive(false);
+                mainMenu.gameObject.SetActive(true);
+            }
+            if (mainMenu.gameObject.activeSelf == false && infoMenu.gameObject.activeSelf == true)
+            {
+                infoMenu.gameObject.SetActive(false);
+                mainMenu.gameObject.SetActive(true);
+            }
+        }
     }
 }
